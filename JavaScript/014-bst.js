70d991efd5f8f7540f1069af4181c8d7e430e6c6
anwar <smmaniac@hotmail.co.uk>
2023-02-20 18:03:17 +0000

    Kata 14 complete for JavaScript

--- JavaScript/014-bst.js
+++ JavaScript/014-bst.js
@@ -41,8 +41,37 @@ class Node {
             return this[highOrLow].contains(value);
         }
     }
+
+    locate(value) {
+        if (isNaN(value)) {
+            return;
+        }
+
+        if (value === this.value) {
+            return "";
+        }
+
+        let highOrLow = value > this.value ? "larger" : "smaller";
+        if (this[highOrLow] == null) {
+            return;
+        } else {
+            const pathString = highOrLow + "." + this[highOrLow].locate(value);
+            console.log(pathString);
+            return pathString.replace(/\.$/, "");
+        }
+    }
 }
 
-class BST extends Node {}
+class BST extends Node {
+    replace(node) {
+        if (node instanceof Node) {
+            this.value = node.value;
+            this.larger = node.larger;
+            this.smaller = node.smaller;
+        } else {
+            throw new Error("replace() only accepts arguments of type Node");
+        }
+    }
+}
 
 module.exports = { Node, BST };
